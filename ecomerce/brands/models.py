443d1c71a90541b86dfdaf7f5df07726ae6dea5f
root <h.ameen@xware>
2022-01-17 14:43:37 +0200

    edit model of modules

--- ecomerce/brands/models.py
+++ ecomerce/brands/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 class Brand(models.Model):
     name = models.CharField(max_length=50)
     image = models.FileField(upload_to="uploads/", max_length=100)
-    user = models.ForeignKey(User,related_name='brand_user', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='brand_user', on_delete=models.CASCADE)
