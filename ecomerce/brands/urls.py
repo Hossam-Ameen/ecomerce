cff51b60744cdea755bcc7daef28e7f31c659067
h.ameen <h.ameen>
2022-01-18 13:39:42 +0200

    rmove database

--- ecomerce/brands/urls.py
+++ ecomerce/brands/urls.py
@@ -2,9 +2,6 @@ from django.urls import path,include
 from . import views
 from rest_framework.routers import DefaultRouter
 
-# router = DefaultRouter()
-# router.register(r'brands/', views.BrandViewSet, basename="brands")
-
 router = DefaultRouter()
 router.register(r'brands', views.BrandViewSet, basename="brands")
 urlpatterns = router.urls
