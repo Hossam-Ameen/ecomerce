edb8ac7c5a59016d2d7eb5041f7a8091d9dcc2ed
h.ameen <h.ameen>
2022-01-18 13:47:56 +0200

    add __int__ file in gitignore

--- ecomerce/brands/models.py
+++ ecomerce/brands/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Brand(models.Model):
     name = models.CharField(max_length=50)
     image = models.FileField(upload_to="uploads/", max_length=100)
